3b34a9544e4e64905f7963e3129e1ae6ac19f66a
non209 <143562722+non209@users.noreply.github.com>
2023-11-22 17:49:20 -0600

    Add files via upload

---
+++ 3RA_1_FUNCIONES/src/pkg3ra_1_funciones/Main.java
@@ -0,0 +1,21 @@
+
+package pkg3ra_1_funciones;
+
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
+public class Main {
+  
+    public static void main(String[] args) {
+        imprimirMensaje("Hola mundo :)     (:", 5);
+      
+        
+    }
+    public static void imprimirMensaje(String mensaje, int cant) {
+        for (int i = 0; i < cant; i++) {
+    System.out.println(mensaje);
+           
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        }
+        }
+}
+
